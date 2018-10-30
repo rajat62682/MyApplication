af0b733a36a72b152ad7b0037985358100085ffc
Rajat <rajat.r@BRILLIO.COM>
2018-10-30 17:44:16 +0530

    Change in Home controller for filter function

--- src/app/controllers/home.controller.js
+++ src/app/controllers/home.controller.js
@@ -1,20 +1,39 @@
 
-(function() {
-    'use strict';
-  
-    angular
-      .module('myApplication')
-      .controller('HomeController', HomeController);
-  
-    /** @ngInject */
-    function HomeController($scope,$http) {
-      var _this = this;
-      $scope.myData=[];
-      $http.get('json/home.json').success(function(data){
-         $scope.myData=data;
-        });
+(function () {
+  'use strict';
+
+  angular
+    .module('myApplication')
+    .controller('HomeController', HomeController);
+
+  /** @ngInject */
+  function HomeController($scope, $http) {
+    var _this = this;
+    $scope.myData = [];
+    $scope.newData = [];
+    $http.get('json/home.json').success(function (data) {
+      $scope.myData = data;
+      $scope.newData = angular.copy(data);
+    });
+
+    this.search = function () {
+      $scope.newData = angular.copy($scope.myData.filter(checkValue));
 
     }
+    function checkValue(item) {
 
-  })();
-  
+      if (item.name.toLowerCase().includes($scope.myInput.toLowerCase())) {
+        return true;
+      } else if (item.email.toLowerCase().includes($scope.myInput.toLowerCase())) {
+        return true;
+      }
+      else if (item.phone.toLowerCase().includes($scope.myInput.toLowerCase())) {
+        return true;
+      }
+
+      return false;
+
+    }
+  }
+
+})();
